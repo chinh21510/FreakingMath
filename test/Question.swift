f3fff474fce10e9ab5273ee88bd2184b27f1d62d
Chinh <dinhcongchinh1203@gmail.com>
2020-04-16 15:36:00 +0700

    commit

--- test/Question.swift
+++ test/Question.swift
@@ -11,4 +11,5 @@ import Foundation
 struct Question {
     let description: String
     let isTrue: Bool
+    var timeLeft = 2.0
 }
--- test/ViewController.swift
+++ test/ViewController.swift
@@ -14,18 +14,31 @@ class ViewController: UIViewController {
     var timer = Timer()
     var scores: [Int] = []
     var checkIf = true
+    let relativeFontConstain1: CGFloat = 0.08
+    let relativeFontConstain2: CGFloat = 0.04
     
     @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var whenLoseUIView: UIImageView!
     @IBOutlet weak var gameOverLabel: UILabel!
     @IBOutlet weak var newScoreLabel: UILabel!
     @IBOutlet weak var bestScoreLabel: UILabel!
     @IBOutlet weak var score: UILabel!
     @IBOutlet weak var timeProgress: UIProgressView!
-
+    @IBOutlet weak var replayUIButton: UIButton!
+    
+    @IBOutlet weak var trueImageView: UIImageView!
+    
+    @IBOutlet weak var falseImageView: UIImageView!
+    
+    
+    @IBOutlet weak var falseUIButton: UIButton!
+    
+    @IBOutlet weak var gameOverUIview: UIView!
+    
+    @IBOutlet weak var trueUIButton: UIButton!
     
     
     func newQuestion() -> Question{
+        
         let lhs = Int.random(in: 0...10)
         let rhs = Int.random(in: 0...10)
         let isTrue = Bool.random()
@@ -40,24 +53,24 @@ class ViewController: UIViewController {
             description = "\(lhs)+\(rhs)\n=\(lhs + rhs + offset)\n "
         }
         label.text = "\(description)"
-        return Question(description: description, isTrue: isTrue)
+        return Question(description: description, isTrue: isTrue, timeLeft: 2)
     }
-    func hidden() {
+    func lose() {
         let a = Int(score.text!)!
         var bestScore = 0
         label.isHidden = true
-        whenLoseUIView.isHidden = false
-        gameOverLabel.isHidden = false
-        newScoreLabel.isHidden = false
-        newScoreLabel.text = "New \(a)"
+        gameOverUIview.isHidden = false
         scores.append(a)
-        bestScoreLabel.isHidden = false
+        newScoreLabel.text = "New \(a)"
         for score in scores {
             if bestScore < score {
                 bestScore = score
             }
         }
-        bestScoreLabel.text = "Best: \(bestScore)"
+        bestScoreLabel.text = "Best \(bestScore)"
+        replayUIButton.isHidden = false
+        trueUIButton.isUserInteractionEnabled = false
+        falseUIButton.isUserInteractionEnabled = false
     }
     @IBAction func trueButtonDidTap(_ sender: Any) {
        let a = Int(score.text!)!
@@ -68,9 +81,8 @@ class ViewController: UIViewController {
             timer.invalidate()
             times()
        } else {
-            hidden()
-            score.text = "0"
-            timeProgress.progress = 0
+            lose()
+            timer.invalidate()
        }
     }
     @IBAction func falseButtonDidTap(_ sender: Any) {
@@ -82,43 +94,68 @@ class ViewController: UIViewController {
             timer.invalidate()
             times()
         } else {
-            hidden()
-            score.text = "0"
-            timeProgress.progress = 0
+            lose()
+            timer.invalidate()
         }
- 
     }
-    
- 
     @IBAction func replay(_ sender: Any) {
-        self.question = newQuestion()
         label.isHidden = false
-        gameOverLabel.isHidden = true
-        newScoreLabel.isHidden = true
-        bestScoreLabel.isHidden = true
-        whenLoseUIView.isHidden = true
-        
+        timer.invalidate()
+        viewDidLoad()
     }
-    
-        
-    
     func times() {
         timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: true)
     }
+    
     @objc func runTimedCode() {
-        timeProgress.progress -= 0.001
+        question.timeLeft = question.timeLeft - 0.01
+        timeProgress.progress = Float(question.timeLeft / 2.0)
         if timeProgress.progress == 0 {
-            hidden()
-            score.text = "0"
+            lose()
         }
+       
+    }
+    func createShadow(view: UIView, red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat, width: CGFloat, height: CGFloat, ratioCornerRadius: CGFloat, masks: Bool) {
+        view.center = self.view.center
+        view.layer.shadowColor = UIColor(red: red, green: green, blue: blue, alpha: alpha).cgColor
+        view.layer.shadowOffset = CGSize(width: width, height: height)
+        view.layer.shadowOpacity = 1.0
+        view.layer.shadowRadius = 0
+        view.layer.cornerRadius = view.bounds.size.width / ratioCornerRadius
+        view.layer.masksToBounds = masks
     }
     override func viewDidLoad() {
+        timeProgress.progress = 1
         super.viewDidLoad()
         self.question = newQuestion()
-        whenLoseUIView.isHidden = true
-        gameOverLabel.isHidden = true
-        newScoreLabel.isHidden = true
-        bestScoreLabel.isHidden = true
+        score.text = "0"
+        gameOverUIview.isHidden = true
+        replayUIButton.isHidden = true
+        trueUIButton.isUserInteractionEnabled = true
+        falseUIButton.isUserInteractionEnabled = true
+        
+        let playIcon = UIImage(named: "playIcon-1")
+        let playIconImage = playIcon?.withRenderingMode(.alwaysTemplate)
+        replayUIButton.setImage(playIconImage, for: .normal)
+        replayUIButton.tintColor = .blue
+        
+        createShadow(view: gameOverUIview, red: 0.0, green: 0.0, blue: 0.0, alpha: 0.6, width: 0, height: 5.0, ratioCornerRadius: 40, masks: false)
+        createShadow(view: replayUIButton, red: 1, green: 1, blue: 1, alpha: 0.8, width: 0.0, height: 8.0, ratioCornerRadius: 20, masks: false)
+        createShadow(view: trueUIButton, red: 1, green: 1, blue: 1, alpha: 0.8, width: 0.0, height: 8.0, ratioCornerRadius: 20, masks: false)
+        createShadow(view: falseUIButton, red: 1, green: 1, blue: 1, alpha: 0.8, width: 0.0, height: 8.0, ratioCornerRadius: 20, masks: false)
+       
+        trueImageView.layer.cornerRadius = trueImageView.bounds.size.width / 20
+        trueImageView.clipsToBounds = true
+        
+        
+        falseImageView.layer.cornerRadius = falseImageView.bounds.size.width / 20
+        falseImageView.clipsToBounds = true
+        
+        
+        label.font = label.font.withSize(self.view.frame.height * relativeFontConstain1)
+        gameOverLabel.font = gameOverLabel.font.withSize(self.view.frame.height * relativeFontConstain2)
+        newScoreLabel.font = newScoreLabel.font.withSize(self.view.frame.height * relativeFontConstain2)
+        bestScoreLabel.font = bestScoreLabel.font.withSize(self.view.frame.height * relativeFontConstain2)
+        
     }
 }
-
